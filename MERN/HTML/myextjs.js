5afedb7f2d1d273f6670a51f601eaad7bc9d2886
Vupallancha sai vershith <65228661+saivershith@users.noreply.github.com>
2021-12-27 17:48:17 +0530

    Delete MERN directory

--- MERN/HTML/myextjs.js
+++
@@ -1,6 +0,0 @@
-function func(){
-    let v=document.getElementById('mytext').value;
-   document.getElementById('myh3').textContent="Welcome"+v+" !! "
-   document.getElementById("myimg").style="dispaly:none"
-    
-}
